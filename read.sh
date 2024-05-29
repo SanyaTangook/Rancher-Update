3c0bef30eaa4d6f898ff95e4915a8d7e497de80f
Sanya <93518732+SanyaTangook@users.noreply.github.com>
2024-05-29 15:31:49 +0700

    Update read.sh

--- read.sh
+++ read.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/bash
 echo "ServiceAccount"
 echo "-----------------"
-kubectl  -n cattle-system get ServiceAccount | grep --color 'rancher'
+kubectl  -n cattle-system get ServiceAccount | grep -w --color 'rancher'
 echo "-----------------"
 
 echo "ConfigMap"
 echo "-----------------"
-kubectl  -n cattle-system get ConfigMap | grep --color 'rancher-config'
+kubectl  -n cattle-system get ConfigMap | grep -w --color 'rancher-config'
 echo "-----------------"
 
 echo "ClusterRoleBinding"
 echo "-----------------"
-kubectl  -n cattle-system get ClusterRoleBinding  | grep --color 'rancher'
+kubectl  -n cattle-system get ClusterRoleBinding  | grep -w --color 'rancher'
 echo "-----------------"
 
 echo "Deployment"
 echo "-----------------"
-kubectl  -n cattle-system get Deployment | grep --color 'rancher'
+kubectl  -n cattle-system get Deployment | grep -w --color 'rancher'
 echo "-----------------"
 
 echo "Service"
 echo "-----------------"
-kubectl  -n cattle-system get svc | grep --color 'rancher'
+kubectl  -n cattle-system get svc | grep -w --color 'rancher'
 echo "-----------------"
 
 echo "Issuer"
 echo "-----------------"
-kubectl  get Issuer | grep --color 'rancher'
+kubectl  get Issuer | grep -w --color 'rancher'
 echo "-----------------"
 
 
 read update
 
-if [$update == "yes"] 
+if [[$update == "yes"]] 
 then
     curl -o rancher-update.yaml https://raw.githubusercontent.com/SanyaTangook/Rancher-Update/main/rancher-update.yaml
 else
