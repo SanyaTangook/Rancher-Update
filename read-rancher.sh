9c136df6af9429943c68f557f3229997e857b317
SanyaTangook <champmc123@gmail.com>
2024-05-30 11:49:47 +0700

    Edit bash

--- read-rancher.sh
+++ read-rancher.sh
@@ -2,34 +2,34 @@
 echo "ServiceAccount"
 echo "-----------------"
 kubectl  -n cattle-system get ServiceAccount | grep -w --color 'rancher'
-echo "-----------------"
+echo "-------end-------"
 
 echo "ConfigMap"
 echo "-----------------"
 kubectl  -n cattle-system get ConfigMap | grep -w --color 'rancher-config'
-echo "-----------------"
+echo "-------end-------"
 
 echo "ClusterRoleBinding"
 echo "-----------------"
 kubectl  -n cattle-system get ClusterRoleBinding  | grep -w --color 'rancher'
-echo "-----------------"
+echo "-------end-------"
 
 echo "Deployment"
 echo "-----------------"
 kubectl  -n cattle-system get Deployment | grep -w --color 'rancher'
-echo "-----------------"
+echo "-------end-------"
 
 echo "Service"
 echo "-----------------"
 kubectl  -n cattle-system get svc | grep -w --color 'rancher'
-echo "-----------------"
+echo "-------end-------"
 
 echo "Issuer"
 echo "-----------------"
 kubectl  get Issuer | grep -w --color 'rancher'
-echo "-----------------"
+echo "-------end-------"
 
-echo "input Yes to Update"
+echo "input yes to Update"
 read update
 
 if [[ $update == "yes" ]] 
